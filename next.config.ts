93ad4ce56fa95143a96c915c9a3a5f130cbd507a
Cyrille <cyrille.burtin@me.com>
2025-02-21 11:56:58 +0100

    [FEAT] get game API token

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        hostname: "*.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
