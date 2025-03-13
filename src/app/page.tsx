7e499ceadbce702fc66054670eb128158c286181
Cyrille <cyrille.burtin@me.com>
2025-03-13 14:05:20 +0100

    [chore] clean APP of old files and folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import GameList from '@/features/gameList/GameList';
 import { Suspense } from 'react';
 
 export default async function Home() {
   return (
     <Suspense fallback={'loading...'}>
-      <GameList />
+      <p>accueil </p>
     </Suspense>
   );
 }
